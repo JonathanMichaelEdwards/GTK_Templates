fa871e18afea098ffe191173c5c231a0afb97fa4
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2020-11-17 00:18:05 +1300

    tidy code

--- Template - V2/src/createWindow.cpp
+++ Template - V2/src/createWindow.cpp
@@ -1,8 +1,8 @@
 #include <gtk/gtk.h>
 #include "createWindow.h"
 
-#define LEN_OF_FILE(NAME) 18 + strlen(NAME)
 
+#define LEN_OF_FILE(NAME) 18 + strlen(NAME)
 
 
 
@@ -15,11 +15,6 @@ Widget *builderGetObject(GtkBuilder *builder, Widget *widgets, const char *names
     return widgets;
 }
 
-// char *fileLocation(char *findLoc) 
-// {
-//     return realpath(findLoc, NULL);  // Find the absolute path
-// }
-
 
 void createWindow(Widget *widgets, const char *names[], const char *title, int size)
 {
@@ -29,14 +24,6 @@ void createWindow(Widget *widgets, const char *names[], const char *title, int s
     char *fileG = (char*)malloc(sizeof(char) * LEN_OF_FILE(names[0]));
     sprintf(fileG, "../glade/%s.glade", names[0]);
     GtkBuilder *builder = gtk_builder_new_from_file(fileG);
-
-    // char *loc = fileLocation("../glade/template.glade");
-
-//     // Link the Glade GUI builder
-//     // char *fileG = (char*)malloc(sizeof(char) * LEN_OF_FILE(WidgetNames[0]));
-//     // sprintf(fileG, "glade/%s.glade", WidgetNames[0]);
-//     // puts(loc);
-    // GtkBuilder *builder = gtk_builder_new_from_file(loc);
 
     // Create the default Window
     GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, names[0]));
@@ -54,6 +41,6 @@ void createWindow(Widget *widgets, const char *names[], const char *title, int s
     gtk_main();
     
     // Free memory
-    // free(fileG);
+    free(fileG);
     g_slice_free(Widget, widgets);
 }
